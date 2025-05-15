d32399144020ef01ebe77f38d6221977f547c6a8
Kai Kratz <Kai.Kratz@ecmwf.int>
2025-05-15 16:10:29 +0100

    Add support to publish html to sites
    Adds a few new reusable workflows:
    - Add pr-preview-publish workflow
      -> Allows publishing a build output to a temporary location
      -> Updates PR description woth link to output
    - Add pr-preview-un-publish workflow
      -> Removes temporarily published output
    - Add doc-publish workflow
      -> Published a build output to sites
      -> Optionally allows to create softlinks (e.g. latest/stable)
      -> Supposed to be used on push to develop/master/main or on tag
    
    Adds a few new composite actions:
    - Add new version selector action
    - Add build & cache doxygen action
    - Add Update PR-desc action
    
    See workflows / actions for more details & documentation.
    
    Co-authored-by: Tobias Kremer <tobias.kremer@ecmwf.int>

--- ecmwf-sites-upload/upload.py
+++ ecmwf-sites-upload/upload.py
@@ -1,13 +1,14 @@
 import argparse
 import sys
+import os
 
 from sites.sdk import SitesClient
 from sites.sdk.sites import Site, Authenticator
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--token", type=str)
 parser.add_argument("--path", type=str)
+parser.add_argument("--remote-path", type=str, default=".")
 parser.add_argument("--space", type=str)
 parser.add_argument("--name", type=str)
 
@@ -23,14 +24,16 @@ if error:
     sys.exit(1)
 
 # setup authenticated content manager instance
-site_authenticator = Authenticator.from_token(token=args.token)
+site_authenticator = Authenticator.from_token(token=os.environ["SITES_TOKEN"])
 client = SitesClient(authenticator=site_authenticator)
 site = Site.from_space_and_name(space=args.space, name=args.name)
 site_content_manager = client.content(site=site)
 
 # upload all the contents of a directory
 print("Uploading...")
-res = site_content_manager.upload(local_path=args.path, recursive=True)
+res = site_content_manager.upload(
+    local_path=args.path, remote_path=args.remote_path, recursive=True
+)
 print(res)
 if isinstance(res, list) and len(res):
     print(f"Successfully uploaded to sites.ecmwf.int/{args.space}/{args.name}")
