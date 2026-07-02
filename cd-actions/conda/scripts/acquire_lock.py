8a3da948b3df1d21e90185dfc9a079c14a4d0dd1
Jakub Recman <jakub@oxidian.com>
2026-07-02 15:17:40 +0200

    Final test

--- cd-actions/conda/scripts/acquire_lock.py
+++ cd-actions/conda/scripts/acquire_lock.py
@@ -104,7 +104,7 @@ def main():
         f"/repos/{LOCK_REPO}/actions/workflows/{LOCK_WORKFLOW}/dispatches",
         method="POST",
         data={
-            "ref": "v2",
+            "ref": "cd-conda-multi-platform",
             "inputs": {
                 "nexus_url": args.nexus_url,
                 "nexus_token": args.nexus_token,
